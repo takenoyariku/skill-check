11c6796a444e3b76e2f0b876eccb4b767977f283
takenoya <tknyr.born0530@outlook.jp>
2024-04-26 14:42:28 +0900

    [update]新規作成ページ フロントコーディング

--- resources/views/diary/create.blade.php
+++ resources/views/diary/create.blade.php
@@ -2,9 +2,36 @@
 
 @section('title', '新規作成ページ')
 @section('style')
-  @vite(['resources/scss/create.scss'])
+  @vite(['resources/scss/diary/create.scss'])
 @endsection
 
 @section('content')
-
+  <div class="l-main">
+    <h1>新規作成</h1>
+    <form action="{{ route('diary.store') }}" enctype="multipart/form-data">
+      @csrf
+      <table>
+        <tbody>
+          <tr>
+            <th>
+              <label for="image">サムネイル画像</label>
+            </th>
+            <td>
+              <input id="image" type="file" name="image" accept=".jpg, .jpeg">
+              <p>※jpgのみアップロード可能です</p>
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <label for="content">本文</label>
+            </th>
+            <td>
+              <input id="content" type="text">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <button type="submit">登録する</button>
+    </form>
+  </div>
 @endsection
