d2f9124d950b236932ba50794fbdfea904ab2432
takenoya <tknyr.born0530@outlook.jp>
2024-05-03 15:29:16 +0900

    [fix]画像削除処理 不要な引数を削除

--- app/Utils/ImageDestroy.php
+++ app/Utils/ImageDestroy.php
@@ -8,10 +8,9 @@ class ImageDestroy
 {
   /**
    * 画像をローカルストレージから削除
-   * @param \Illuminate\Http\Request
    * @param string $image_name 対象画像パス
    */
-  public function destroyImage($request, $image_name): void
+  public function destroyImage($image_name): void
   {
     Storage::disk('public')->delete('uploads/'.$image_name);
   }
